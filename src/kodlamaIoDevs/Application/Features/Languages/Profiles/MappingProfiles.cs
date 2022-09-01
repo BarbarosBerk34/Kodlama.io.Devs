ce64da34afdf2455b026a573e5159a6f867d8d58
BarbarosBerk34 <barbarosberk34@hotmail.com>
2022-09-01 19:23:52 +0300

    Add CQRS, Business Rules & Global Exception Handling

---
+++ src/kodlamaIoDevs/Application/Features/Languages/Profiles/MappingProfiles.cs
@@ -0,0 +1,21 @@
+﻿using Application.Features.Languages.Commands.CreateLanguage;
+using Application.Features.Languages.Dtos;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Languages.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Language, CreatedLanguageDto>().ReverseMap();
+            CreateMap<Language, CreateLanguageCommand>().ReverseMap();
+        }
+    }
+}
---
+++ src/kodlamaIoDevs/WebAPI/Controllers/LanguageController.cs
@@ -0,0 +1,23 @@
+﻿using Application.Features.Languages.Commands.CreateLanguage;
+using Application.Features.Languages.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LanguageController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateLanguageCommand createLanguageCommand)
+        {
+            CreatedLanguageDto result = await Mediator.Send(createLanguageCommand);
+            return Created("", result);
+        }
+    }
+}
